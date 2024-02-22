f1ff981e4dd85ecf37d357ab3e1c2210cd12499b
alamsyah10 <alamsyah_imanudin0209@gmail.com>
2024-02-22 11:41:44 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import LoginPage from './assets/pages/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />}>
+          {/* <Route index element={<Home />} /> */}
+          <Route path="login" element={<LoginPage />} />
+          {/* <Route path="contact" element={<Contact />} /> */}
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/assets/pages/Login/Login.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './LoginPage.css'; // Import custom styles
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginStatus, setLoginStatus] = useState('');
+  const navigate = useNavigate(); // Initialize useHistory hook
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:8081/api/v2/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (!response.ok) {
+        setLoginStatus('Invalid username or password. Please try again.');
+      } else {
+        const responseData = await response.json();
+        const {token, refreshToken, message} = responseData;
+  
+        if (message != null) {
+          setLoginStatus(message);
+        } else {
+          setLoginStatus('Login successful');
+          navigate('/profile',{ state: { token: token, refreshToken: refreshToken }});
+        }
+      }
+    } catch (error) {
+      // Handle login error
+      setLoginStatus('Catch an error: ' + error.message);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      {loginStatus && (
+        <div className="alert alert-failed">
+          <p>{loginStatus}</p>
+        </div>
+      )}
+      <div className="login-header">Login</div>
+      <form className="login-form" onSubmit={handleLogin}>
+        <input
+          className="input-field"
+          type="email"
+          id="email"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          className="input-field"
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button className="login-button" type="submit">
+          Login
+        </button>
+      </form>
+      <div className="signup-text">
+        Don't have an account? <a className="signup-link" href="/register">Sign up</a>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
